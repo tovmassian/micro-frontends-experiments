def5e4acce7b8a923d72f9d0b82cdd57b7553946
tovmassian <khachatur.tovmasyan@gmail.com>
2020-11-01 21:18:30 +0400

    Add trainings and do some cleanup

---
+++ micro-frontends/trainings/src/main.js
@@ -0,0 +1,32 @@
+import './set-public-path';
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial);
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+        },
+      });
+    },
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
---
+++ micro-frontends/trainings/src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@tovmassian/trainings", 2);
