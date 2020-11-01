1b708f20613886c44756eaec008d8b1189628d7c
tovmassian <khachatur.tovmasyan@gmail.com>
2020-11-01 22:03:35 +0400

    fix annotations project directory name

--- micro-frontends/annotations/webpack.config.js
+++ micro-frontends/annotations/webpack.config.js
@@ -1,5 +1,6 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const path = require('path');
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
--- root-config/startDevFull.sh
+++ root-config/startDevFull.sh
@@ -6,4 +6,4 @@ npm start --prefix ./../micro-frontends/navbar &
 
 npm start --prefix ./../micro-frontends/annotations &
 
-npm start --prefix ./../micro-frontends/trainings &
+npm start --prefix ./../micro-frontends/trainings
